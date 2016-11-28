106fb31bdddec65bae8807d3b92743a0f90b2ead
muyingchen <muyingchen@berkeley.edu>
2016-11-28 14:21:35 -0800

    changed app name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]#not sure if necessary
   before_action :correct_user,   only: [:edit, :update]
-  
+
 
 
   def index
@@ -20,11 +20,11 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-  def create 
+  def create
   	@user = User.new(user_params)
   	if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to TRUMP?!"
       redirect_to user_url(@user)
 	   else
 		  render 'new'
